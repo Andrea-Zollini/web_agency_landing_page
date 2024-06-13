307e6472f66e61c73fe0b6d6c4b82277955f5926
andrea <andrea.zollini@proton.me>
2024-06-13 10:59:03 +0200

    style setup + layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'soft-red': '#fe7867',
+			'yellow': '	#fad400',
+			'dark-cyan': '#25564b',
+			'dark-blue': '#19536b',
+			'moderate-cyan': '#458c7e',
+			'desaturated-blue': '#23303e',
+			'v-d-grayish-blue': '#5a636c',
+			'd-grayish-blue': '#818498',
+			'grayish-blue': '#a7abae',
+			'white': '#ffffff',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
